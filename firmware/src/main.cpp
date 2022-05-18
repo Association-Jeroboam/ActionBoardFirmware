74da9b6eb83fbc4123c2d644091c6788f4a57b80
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2022-05-18 17:16:34 +0200

    Cyphal heartbeat

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -10,8 +10,46 @@
 //#include "PCA9635.h"
 //#include "Adafruit_PWMServoDriver.h"
 #include "hal_pal.h"
+#include "Heartbeat_1_0.h"
 
 static THD_WORKING_AREA(waShellThread, SHELL_WA_SIZE);
+
+void cyphalHeartBeatRoutine() {
+    static CanardTransferID transfer_id = 0;
+    static uint16_t MSB = 0;
+    static uint32_t before = 0;
+    uint32_t now = chVTGetSystemTime();
+    if(now <= before) {
+        MSB++;
+    }
+    const uavcan_node_Heartbeat_1_0 heartbeat = {
+            .uptime = TIME_I2S(now | (MSB << 16)),
+            .health = {
+                    .value = uavcan_node_Health_1_0_NOMINAL
+            },
+            .mode = {
+                    .value = uavcan_node_Mode_1_0_OPERATIONAL,
+            },
+            .vendor_specific_status_code = 42,
+    };
+
+    size_t buf_size = uavcan_node_Heartbeat_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_;
+    uint8_t buffer[uavcan_node_Heartbeat_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_];
+
+    uavcan_node_Heartbeat_1_0_serialize_(&heartbeat, buffer, &buf_size);
+
+
+    const CanardTransferMetadata metadata = {
+            .priority = CanardPriorityNominal,
+            .transfer_kind = CanardTransferKindMessage,
+            .port_id = uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_,
+            .remote_node_id = CANARD_NODE_ID_UNSET,
+            .transfer_id = transfer_id,
+    };
+    transfer_id++;
+    Board::Com::CANBus::send(&metadata, buf_size,  buffer);
+    before = now;
+}
 
 int main() {
     halInit();
@@ -31,7 +69,8 @@ int main() {
     while (!chThdShouldTerminateX()) {
         Board::IO::toggleNucleoLed();
         Board::IO::getResistanceMeasure();
-        chThdSleepMilliseconds(100);
+        cyphalHeartBeatRoutine();
+        chThdSleepMilliseconds(1000);
     }
     Logging::println("Shutting down");
 }
