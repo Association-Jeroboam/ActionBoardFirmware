98a031ac036a262180dad12defa28fa9a013e7e8
Apache3 <tj.thomas.jeantet@gmail.com>
2023-05-15 17:59:59 +0200

    Longer turbine timeouts

--- firmware/src/PeripheralManager.hpp
+++ firmware/src/PeripheralManager.hpp
@@ -5,8 +5,8 @@
 #include "CanListener.hpp"
 
 constexpr uint32_t PNEUMATICS_VALVE_ENABLED_TIMEOUT_MS = 500;
-constexpr uint32_t TURBINE_MAX_SPEED_TIMEOUT_MS = 10000;
-constexpr uint32_t TURBINE_MAX_ENABLED_TIMEOUT_MS = 60000;
+constexpr uint32_t TURBINE_MAX_SPEED_TIMEOUT_MS = 50000;
+constexpr uint32_t TURBINE_MAX_ENABLED_TIMEOUT_MS = 100000;
 
 class PeripheralManager:public CanListener {
 public:
